28450e2c5cd07b7fb30a1c07ad874fab590ef33e
arafat248 <aab.arafat248@gmail.com>
2026-05-04 19:58:37 +0600

    added api root view

--- shop248/urls.py
+++ shop248/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from debug_toolbar.toolbar import debug_toolbar_urls
+from .views import api_root_view
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
@@ -23,6 +24,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', api_root_view),
     path('api-auth/', include('rest_framework.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
